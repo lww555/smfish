ae0e4a3cb595440da721160c9c77bbc9927d2b20
zenker <zenker_w@163.com>
2016-08-20 22:16:08 +0800

    life

--- src/scripts/views/life.js
+++ src/scripts/views/life.js
@@ -1,4 +1,17 @@
 var lifeTpl = require('../tpls/life.string');
 SPA.defineView("life",{
-	html:lifeTpl
+	html:lifeTpl,
+	bindEvents:{
+		"ready":function(){
+			var mySwiper = new Swiper ('#life_swiper',{
+				onSlideChangeStart: function(swiper){
+					var index=swiper.activeIndex;
+					$(".life_nav span").eq(index).addClass("active").siblings().removeClass("active")
+				}
+			})
+			$(".life_nav span").on('tap', function () {
+				mySwiper.slideTo($(this).index());
+			});
+		}
+	}
 })
