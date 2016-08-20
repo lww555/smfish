f467efc019f1295f406178f9a0de7301a68dbd5b
lww555 <657566433@qq.com>
2016-08-20 11:33:12 +0800

    index

--- src/scripts/views/home.js
+++ src/scripts/views/home.js
@@ -1,4 +1,18 @@
 var homeTpl = require('../tpls/home.string');
 SPA.defineView("home",{
-	html:homeTpl
+	html:homeTpl,
+	// 绑定视图事件
+	bindEvents:{
+		'ready': function (){
+		 	var mySwiper = new Swiper ('.swiper-container', {
+		    direction:'horizontal',
+		    loop: true,
+		    autoplay: 3000,
+		    // 如果需要分页器
+		    pagination:'.swiper-pagination',
+		    paginationClickable: true,
+		    autoplayDisableOnInteraction :false
+		  });  
+		}
+	}
 })
