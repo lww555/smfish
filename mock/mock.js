e21bf9c9139f1c4aac2ebb8481c14eeb1c70510d
zenker <zenker_w@163.com>
2016-08-21 15:44:23 +0800

    life

---
+++ mock/mock.js
@@ -0,0 +1,8 @@
+module.exports = {
+    rules:[
+        {
+            pattern: /\/api\/livelist\.php\?type=eat\&pageNo=1/,
+            respondwith: './life_json/eat.josn'
+        },
+    ]
+}
--- src/scripts/views/life.js
+++ src/scripts/views/life.js
@@ -1,8 +1,32 @@
 var lifeTpl = require('../tpls/life.string');
 SPA.defineView("life",{
 	html:lifeTpl,
+	plugins: [{
+		name: 'avalon',
+		options:function(vm){
+			vm.navlist=[];
+			vm.lifecontainer=[];
+			vm.car_navlist=[];
+			vm.car_container=[];
+		}
+	}],
 	bindEvents:{
-		"ready":function(){
+		"show":function(){
+			var vm = this.getVM();
+			$.ajax({
+				url:"/smfish/mock/life_json/eat.json",
+				success:function(msg){
+					vm.navlist=msg.data.icons;
+					vm.lifecontainer=msg.data.life;
+				}
+			})
+			$.ajax({
+				url:"/smfish/mock/life_json/car.json",
+				success:function(msg){
+					vm.car_navlist=msg.data.icons;
+					vm.car_container=msg.data.life;
+				}
+			})
 			var mySwiper = new Swiper ('#life_swiper',{
 				onSlideChangeStart: function(swiper){
 					var index=swiper.activeIndex;
